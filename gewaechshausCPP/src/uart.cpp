1004daafe4f819f612b4937c61b2f7812a41a884
René Galow <rensky.g@googlemail.com>
2014-11-20 19:23:55 +0100

    change boud rate for xbee

--- gewaechshausCPP/src/uart.cpp
+++ gewaechshausCPP/src/uart.cpp
@@ -200,7 +200,7 @@ void Usart::usart3Init(void)
   GPIO_InitStructure.GPIO_Pin = GPIO_Pin_11;
   GPIO_Init(GPIOB, &GPIO_InitStructure);
 
-  USART_InitStructure.USART_BaudRate = 115200;
+  USART_InitStructure.USART_BaudRate = 9600;
   USART_InitStructure.USART_WordLength = USART_WordLength_8b;
   USART_InitStructure.USART_StopBits = USART_StopBits_1;
   USART_InitStructure.USART_Parity = USART_Parity_No;
